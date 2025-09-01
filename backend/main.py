0054bf8fb946f406d4ab80fe28a2c77f78d3679d
faris-niz <tuk86431@temple.edu>
2025-09-01 12:11:01 -0400

    link analyzer endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from utils.pdf_parser import extract_text_from_pdf
 import random
-from utils.llm_tools import compute_similarity
 from utils.llm_tools import ResumeAnalysis
-from utils.llm_tools import generate_feedback
+from utils.llm_tools import generate_feedback_description
+from utils.llm_tools import generate_feedback_link
 
 
 app = FastAPI()
@@ -18,8 +18,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/analyze", response_model=ResumeAnalysis)
+@app.post("/analyze/description", response_model=ResumeAnalysis)
 async def analyze_resume(resume: UploadFile, job_description: str = Form(...)):
     resume_text = await extract_text_from_pdf(resume)
-    feedback =  await generate_feedback(resume_text, job_description)
+    feedback =  await generate_feedback_description(resume_text, job_description)
+    return feedback
+
+@app.post("/analyze/link", response_model=ResumeAnalysis)
+async def analyze_resume(resume: UploadFile, job_url: str = Form(...)):
+    resume_text = await extract_text_from_pdf(resume)
+    feedback =  await generate_feedback_link(resume_text, job_url)
     return feedback
--- backend/utils/llm_tools.py
+++ backend/utils/llm_tools.py
@@ -6,7 +6,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 import os
 from google import genai
 from langchain_google_genai import ChatGoogleGenerativeAI
-
 from pydantic import BaseModel
 from langchain.prompts import ChatPromptTemplate
 from langchain.output_parsers import PydanticOutputParser
@@ -26,26 +25,39 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.7  # Adjust as desired
 )
 
-prompt = ChatPromptTemplate.from_messages([
+description_prompt = ChatPromptTemplate.from_messages([
     ("system", "You are an expert career advisor. Compare a resume text content with a job description and give structured feedback."),
     ("user", "Resume text content:\n{resume_text}\n\nJob Description:\n{job_description}\n\n"
              "Respond in JSON with fields 'match_score' (0–100) and 'feedback'.\n"
              "{format_instructions}")
 ])
 
-def get_embedding(text: str) -> list:
-    return model.encode(text, convert_to_numpy=True)
+link_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are an expert career advisor. Compare a resume text content with a job url and give structured feedback."),
+    ("user", "Resume text content:\n{resume_text}\n\nJob url:\n{job_url}\n\n"
+             "Respond in JSON with fields 'match_score' (0–100) and 'feedback'.\n"
+             "{format_instructions}")
+])
 
-def compute_similarity(text1: str, text2: str) -> float:
-    emb1 = np.array(get_embedding(text1)).reshape(1, -1)
-    emb2 = np.array(get_embedding(text2)).reshape(1, -1)
-    score = cosine_similarity(emb1, emb2)[0][0]
-    return float(score)
-
-async def generate_feedback(resume_text: str, job_description: str) -> str:
-    formatted_prompt = prompt.format_messages(
+async def generate_feedback_description(resume_text: str, job_description: str) -> str:
+    formatted_prompt = description_prompt.format_messages(
         resume_text=resume_text,
         job_description=job_description,
+        format_instructions=parser.get_format_instructions()
+    )
+
+    response = await llm.ainvoke(formatted_prompt)
+    parsed = parser.parse(response.content)
+
+    return {
+        "match_score": parsed.match_score,
+        "feedback": parsed.feedback
+    }
+
+async def generate_feedback_link(resume_text: str, job_url: str) -> str:
+    formatted_prompt = link_prompt.format_messages(
+        resume_text=resume_text,
+        job_url=job_url,
         format_instructions=parser.get_format_instructions()
     )
 
